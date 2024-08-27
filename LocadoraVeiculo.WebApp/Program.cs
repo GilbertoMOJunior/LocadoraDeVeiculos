4ab557aca5771003702acf7e96c0128e4c6a2db4
gmo204 <gilbertojr204@gmail.com>
2024-08-27 13:31:26 -0300

    Modulo Taxa

--- LocadoraVeiculo.WebApp/Program.cs
+++ LocadoraVeiculo.WebApp/Program.cs
@@ -1,13 +1,16 @@
 using System.Reflection;
 using LocadoraVeiculos.Aplicacao.ModuloGrupoVeiculos;
 using LocadoraVeiculos.Aplicacao.ModuloPlanos;
+using LocadoraVeiculos.Aplicacao.ModuloTaxa;
 using LocadoraVeiculos.Aplicacao.ModuloVeiculo;
 using LocadoraVeiculos.Dominio.GrupoDeVeiculos;
 using LocadoraVeiculos.Dominio.ModuloPlanos;
+using LocadoraVeiculos.Dominio.ModuloTaxa;
 using LocadoraVeiculos.Dominio.ModuloVeiculo;
 using LocadoraVeiculos.Infra.ORM.Compartilhado;
 using LocadoraVeiculos.Infra.ORM.ModuloGrupoDeVeiculos;
 using LocadoraVeiculos.Infra.ORM.ModuloPlanos;
+using LocadoraVeiculos.Infra.ORM.ModuloTaxa;
 using LocadoraVeiculos.Infra.ORM.ModuloVeiculo;
 
 namespace LocadoraVeiculo.WebApp
@@ -23,10 +26,12 @@ namespace LocadoraVeiculo.WebApp
             builder.Services.AddScoped<IRepositorioGrupoDeVeiculos, RepositorioGrupoDeVeiculosOrm>();
             builder.Services.AddScoped<IRepositorioVeiculo, RepositorioVeiculoEmOrm>();
             builder.Services.AddScoped<IRepositorioPlanos, RepositorioPlanosEmOrm>();
+            builder.Services.AddScoped<IRepositorioTaxa, RepositorioTaxaEmOrm>();
             
             builder.Services.AddScoped<ServicoGrupoVeiculos>();
             builder.Services.AddScoped<ServicoVeiculo>();
             builder.Services.AddScoped<ServicoPlanos>();
+            builder.Services.AddScoped<ServicoTaxa>();
 
             builder.Services.AddAutoMapper(cfg =>
             {
